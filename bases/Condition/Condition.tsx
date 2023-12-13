170d218abb7a9c7724d3ae6b90ccb4c16665f19a
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2023-12-13 18:37:11 +0100

    Feat (service) restore added

--- bases/Condition/Condition.tsx
+++ bases/Condition/Condition.tsx
@@ -1,30 +1,38 @@
 import { Check, X } from "react-feather";
 import s from "./Condition.module.scss";
+import { CSSProperties } from "react";
 
-type ConditionProps = {
+export type ConditionProps = {
   state: "OK" | "PENDING" | "ERROR";
-  children: string;
+  children: any;
+  style?: CSSProperties;
 };
 
-function Condition(props: ConditionProps) {
+export default function Condition(props: ConditionProps) {
   const theme = {
     OK: {
-      icon: <Check color="#FFF" />,
+      icon: <Check color="#FFF" size={14} />,
       cls: s["ds-condition--ok"],
+      text: s["ds-condition-text--ok"],
     },
     PENDING: {
-      icon: <Check color="" />,
+      icon: <X color="#78829d" size={14} />,
       cls: s["ds-condition--pending"],
+      text: s["ds-condition-text--pending"],
     },
     ERROR: {
-      icon: <X color="#FFF" />,
+      icon: <X color="#FFF" size={14} />,
       cls: s["ds-condition--error"],
+      text: s["ds-condition-text--error"],
     },
   }[props.state];
 
   return (
-    <div>
-      <div className={`ds-condition-theme`}></div>
+    <div className={s["ds-condition"]} style={props?.style}>
+      <div className={`${s["ds-condition-icon"]} ${theme.cls}`}>
+        {theme.icon}
+      </div>
+      <span className={theme.text}>{props.children}</span>
     </div>
   );
 }
--- pages/auth/restore/new.tsx
+++ pages/auth/restore/new.tsx
@@ -1,24 +1,63 @@
 import Logo from "@/bases/logo";
 import Button from "@/bases/Button/Button";
-import { useState } from "react";
-import { sendRestorePasswordEmail } from "@/services/auth.service";
+import { useEffect, useState } from "react";
+import {
+  restorePassword,
+  sendRestorePasswordEmail,
+} from "@/services/auth.service";
 import { notification } from "antd";
 import { useRouter } from "next/router";
 import { Input } from "@/bases/input";
 import { ArrowLeftOutlined } from "@ant-design/icons";
+import Condition from "@/bases/Condition/Condition";
 
 export default function CreateNewPassword(): JSX.Element {
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>("");
+  const [form, setForm] = useState<{
+    newPassword: string;
+    confirmPassword: string;
+  }>({
+    newPassword: "",
+    confirmPassword: "",
+  });
+  const [conditions, setConditions] = useState<{
+    uppercase: boolean;
+    lowercase: boolean;
+    number: boolean;
+    special: boolean;
+    length: boolean;
+  }>({
+    uppercase: false,
+    lowercase: false,
+    number: false,
+    special: false,
+    length: false,
+  });
+
+  useEffect(() => {
+    const password = form.newPassword;
+
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[^A-Za-z0-9]/.test(password);
+    const isLongEnough = password?.length >= 10;
+    setConditions({
+      uppercase: hasUppercase,
+      lowercase: hasLowercase,
+      number: hasNumber,
+      special: hasSpecialChar,
+      length: isLongEnough,
+    });
+  }, [form.newPassword]);
 
   const handleSignInWithCredentials = async (e: any) => {
     try {
       e?.preventDefault();
       setLoading(true);
-      await sendRestorePasswordEmail(email);
-      notification.success({ message: "Please check your email" });
-      setEmail("");
+      const token: any = router?.query?.t;
+      restorePassword(token, form.newPassword);
       setLoading(false);
     } catch (err: any) {
       console.error(err);
@@ -63,24 +102,82 @@ export default function CreateNewPassword(): JSX.Element {
               placeholder="********"
               type="password"
               label="New password"
-              value={email}
-              onChange={(email) => setEmail((prev) => email.target.value)}
+              value={form.newPassword}
+              onChange={(password) =>
+                setForm((prev) => ({
+                  ...prev,
+                  newPassword: password.target.value,
+                }))
+              }
               style={{ marginBottom: 16 }}
             />
             <Input
               placeholder="********"
               type="password"
               label="Confirm new password"
-              value={email}
-              onChange={(email) => setEmail((prev) => email.target.value)}
+              value={form.confirmPassword}
+              onChange={(password) =>
+                setForm((prev) => ({
+                  ...prev,
+                  confirmPassword: password.target.value,
+                }))
+              }
+              style={{ marginBottom: 16 }}
             />
+            <div>
+              <Condition
+                style={{ marginBottom: 4 }}
+                state={conditions.uppercase ? "OK" : "PENDING"}
+              >
+                One uppercase character
+              </Condition>
+              <Condition
+                style={{ marginBottom: 4 }}
+                state={conditions.lowercase ? "OK" : "PENDING"}
+              >
+                One lowercase character
+              </Condition>
+              <Condition
+                style={{ marginBottom: 4 }}
+                state={conditions.number ? "OK" : "PENDING"}
+              >
+                One number
+              </Condition>
+              <Condition
+                style={{ marginBottom: 4 }}
+                state={conditions.special ? "OK" : "PENDING"}
+              >
+                One special character{" "}
+                <label style={{ color: "#78829d" }}>e.g: {".$?#![](){}"}</label>
+              </Condition>
+              <Condition
+                style={{ marginBottom: 4 }}
+                state={conditions.length ? "OK" : "PENDING"}
+              >
+                10 character minimum
+              </Condition>
+              <Condition
+                state={
+                  form.newPassword != "" &&
+                  form.confirmPassword === form.newPassword
+                    ? "OK"
+                    : "PENDING"
+                }
+              >
+                Both passwords matches
+              </Condition>
+            </div>
             <Button
-              disabled={email == ""}
+              disabled={
+                Object.values(conditions).includes(false) ||
+                form.newPassword === "" ||
+                form.confirmPassword != form.newPassword
+              }
               loading={loading}
               theme="CTA"
               style={{ marginTop: 32 }}
             >
-              Send email
+              Reset password
             </Button>
             <Button
               onClick={() => router.push("/auth/sign-in")}
