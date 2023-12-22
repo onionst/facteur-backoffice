576c2f12a0ddc0023198a3efe506faea5b38c2db
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2023-12-22 13:57:59 +0100

    Fix (UX) password inputs eye-on/off added

--- bases/input.tsx
+++ bases/input.tsx
@@ -1,4 +1,6 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { auto } from "@popperjs/core";
+import { DetailedHTMLProps, InputHTMLAttributes, useState } from "react";
+import { Eye, EyeOff } from "react-feather";
 
 export type InputProps = {
   label?: string;
@@ -11,6 +13,7 @@ export function Input(
   > &
     InputProps
 ) {
+  const [type, setType] = useState(props.type);
   return (
     <div className="w-full">
       {props.label && (
@@ -18,7 +21,36 @@ export function Input(
           {props.label} {props.required ? <span>*</span> : ""}
         </label>
       )}
-      <input {...props} className={`form-control ${props.className || ""}`} />
+      <div className="w-full" style={{ position: "relative" }}>
+        <input
+          {...props}
+          type={type}
+          className={`form-control ${
+            props.type === "password" ? "form-control__input" : ""
+          } ${props.className || ""}`}
+        />
+        {props.type === "password" && (
+          <span
+            onClick={() => {
+              setType((prev) => (prev === "password" ? "text" : "password"));
+            }}
+            style={{
+              position: "absolute",
+              right: 16,
+
+              top: 43,
+              cursor: "pointer",
+              transform: "translateY(-31px)",
+            }}
+          >
+            {type === "password" ? (
+              <Eye color="#4b5675" size={20} />
+            ) : (
+              <EyeOff color="#00986d" size={20} />
+            )}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
