e3be5628255274b6850f50ea80308535a88a8fff
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2024-01-04 12:40:35 +0100

    Feat (Users) manage Reaserchers API KEYs added

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,10 +1,10 @@
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 import s from './Card.module.scss';
 
-export type CardProps = { children: ReactNode; title?: string; titleProps?: any };
+export type CardProps = { children: ReactNode; title?: string; titleProps?: any; style?: CSSProperties };
 export default function Card(props: CardProps) {
   return (
-    <article className={s['ds-card']}>
+    <article className={s['ds-card']} {...props}>
       {props.title && <h4 {...props.titleProps}>{props.title}</h4>}
       <section>{props.children}</section>
     </article>
--- modals/users/EditUser.modal.tsx
+++ modals/users/EditUser.modal.tsx
@@ -1,6 +1,6 @@
-import { Modal, ModalProps } from 'antd';
+import { Modal, ModalProps, Popconfirm, Skeleton, Tabs, Tooltip, notification } from 'antd';
 import { FormEvent, useEffect, useState } from 'react';
-import { X } from 'react-feather';
+import { Copy, Eye, EyeOff, RefreshCw, X } from 'react-feather';
 import s from '../Modals.module.scss';
 import Button from '@/bases/Button/Button';
 import { Input } from '@/bases/Input';
@@ -13,14 +13,20 @@ import Switch from '@/bases/Switch/Switch';
 import { ROLES } from '@/constants/roles.constants';
 import { useAuth } from '@/contexts/auth.context';
 import Wrapper from '@/components/Wrapper/Wrapper';
+import IconButton from '@/bases/IconButton/IconButton';
+import { NOTIFICATIONS_CONFIG } from '@/constants/notifications.constant';
 
 export type EditUserModalProps = {
   id: string;
 };
 export const EditUserModal = (props: EditUserModalProps & ModalProps) => {
   const [loading, setLoading] = useState<boolean>(false);
+  const [loadingApiKey, setLoadingApiKey] = useState<boolean>(false);
+  const [refreshingApiKey, setRefreshingApiKey] = useState<boolean>(false);
+  const [apiKey, setApiKey] = useState<string>('');
+  const [showApiKey, setShowApiKey] = useState<boolean>(false);
   const { fetchUserData } = useUsers();
-  const { session } = useAuth();
+  const { session, getApiCredentials, refreshApiCredentials } = useAuth();
   const [user, setUser] = useState<Partial<User>>({});
 
   const handleUpdateUser = async (e: FormEvent) => {
@@ -36,12 +42,38 @@ export const EditUserModal = (props: EditUserModalProps & ModalProps) => {
     }
   };
 
+  const fetchApiCredentials = async (id: string) => {
+    try {
+      setLoadingApiKey(true);
+      const apiKey = await getApiCredentials(id, 'RESEARCHER');
+      setApiKey(apiKey);
+      setLoadingApiKey(false);
+    } catch (err) {
+      setLoadingApiKey(false);
+    }
+  };
+
+  const handleRefreshApiKey = async () => {
+    try {
+      setRefreshingApiKey(true);
+      const apiKey = await refreshApiCredentials(props.id, 'RESEARCHER');
+      setApiKey(apiKey);
+      setShowApiKey(true);
+      setRefreshingApiKey(false);
+    } catch (err) {
+      setRefreshingApiKey(false);
+    }
+  };
+
   const fetchData = async (id: string) => {
     const data = await fetchUserData(id);
     if (!data) {
       // @ts-ignore
       props.onCancel();
     } else {
+      if (data.role === ROLES.RESEARCHER) {
+        fetchApiCredentials(id);
+      }
       setUser(data);
     }
   };
@@ -55,51 +87,142 @@ export const EditUserModal = (props: EditUserModalProps & ModalProps) => {
     <Modal {...props} closeIcon={<X />} key={user?.id}>
       <ModalHeader subTitle="Edit user" title={user?.name ? `Update ${user?.name}'s information` : 'Update the information of the user'} />
       <form className={s['ds-modal-form']} onSubmit={handleUpdateUser}>
-        <Wrapper>
-          <Input
-            required
-            label="Email"
-            placeholder="User's email"
-            value={user?.email}
-            onChange={v => setUser(prev => ({ ...prev, email: v.target.value }))}
-          />
-          <Input
-            required
-            label="Name"
-            placeholder="User's name"
-            value={user?.name}
-            onChange={v => setUser(prev => ({ ...prev, name: v.target.value }))}
-          />
-          <Input
-            required
-            label="Surname"
-            placeholder="User's surname"
-            value={user?.surname}
-            onChange={v => setUser(prev => ({ ...prev, surname: v.target.value }))}
-          />
-        </Wrapper>
-        <Row align="SPACE">
-          <Card title="2FA">
-            <Switch checked={user?.TFA} onChange={TFA => setUser(prev => ({ ...prev, TFA }))} left="Unactive" right="Active" />
-          </Card>
-          {user?.email != session?.email ? (
-            <Card title="Role">
-              <Switch
-                checked={user?.organizationId ? ROLES.ADMIN === user?.role : ROLES.SUPER_ADMIN === user?.role}
-                onChange={state =>
-                  setUser(prev => ({
-                    ...prev,
-                    role: user?.organizationId ? (state ? ROLES.ADMIN : ROLES.FACT_CHECKER) : state ? ROLES.SUPER_ADMIN : ROLES.RESEARCHER
-                  }))
-                }
-                left={user?.organizationId ? 'Fact-checker' : 'Researcher'}
-                right={user?.organizationId ? 'Administrator' : 'Super administrator'}
+        <Tabs>
+          <Tabs.TabPane key={0} tab="Information">
+            <Wrapper>
+              <Input
+                required
+                label="Email"
+                placeholder="User's email"
+                value={user?.email}
+                onChange={v => setUser(prev => ({ ...prev, email: v.target.value }))}
               />
+              <Input
+                required
+                label="Name"
+                placeholder="User's name"
+                value={user?.name}
+                onChange={v => setUser(prev => ({ ...prev, name: v.target.value }))}
+              />
+              <Input
+                required
+                label="Surname"
+                placeholder="User's surname"
+                value={user?.surname}
+                onChange={v => setUser(prev => ({ ...prev, surname: v.target.value }))}
+              />
+            </Wrapper>
+          </Tabs.TabPane>
+          <Tabs.TabPane key={1} tab="Security">
+            <Card>
+              <Row align="SPACE">
+                <Card title="2FA" style={{ background: '#FFF' }}>
+                  <Switch checked={user?.TFA} onChange={TFA => setUser(prev => ({ ...prev, TFA }))} left="Unactive" right="Active" />
+                </Card>
+                {user?.email != session?.email ? (
+                  <Card title="Role" style={{ background: '#FFF' }}>
+                    <Switch
+                      checked={user?.organizationId ? ROLES.ADMIN === user?.role : ROLES.SUPER_ADMIN === user?.role}
+                      onChange={state =>
+                        setUser(prev => ({
+                          ...prev,
+                          role: user?.organizationId
+                            ? state
+                              ? ROLES.ADMIN
+                              : ROLES.FACT_CHECKER
+                            : state
+                              ? ROLES.SUPER_ADMIN
+                              : ROLES.RESEARCHER
+                        }))
+                      }
+                      left={user?.organizationId ? 'Fact-checker' : 'Researcher'}
+                      right={user?.organizationId ? 'Admin' : 'Super admin'}
+                    />
+                  </Card>
+                ) : (
+                  <div className="w-full" />
+                )}
+              </Row>
             </Card>
-          ) : (
-            <div className="w-full" />
+          </Tabs.TabPane>
+          {user?.role === ROLES.RESEARCHER && (
+            <Tabs.TabPane key={2} tab="API">
+              {' '}
+              {loadingApiKey ? (
+                <Wrapper>
+                  <Skeleton active />
+                </Wrapper>
+              ) : apiKey ? (
+                <Wrapper>
+                  <h5>Researcher's API KEY</h5>
+                  <Row align="SPACE">
+                    <Input
+                      style={{ height: 32, width: '100%', backgroundColor: '#FFF' }}
+                      disabled
+                      value={showApiKey ? apiKey : '****************'}
+                    />
+                    <div>
+                      <Row align="RIGHT">
+                        {!showApiKey && (
+                          <Tooltip title="Show API Key">
+                            <div>
+                              <IconButton type="button" onClick={() => setShowApiKey(true)}>
+                                <Eye color="#252f4a" size={18} />
+                              </IconButton>
+                            </div>
+                          </Tooltip>
+                        )}
+                        {showApiKey && (
+                          <Tooltip title="Hide API Key">
+                            <div>
+                              <IconButton type="button" onClick={() => setShowApiKey(false)}>
+                                <EyeOff color="#252f4a" size={18} />
+                              </IconButton>
+                            </div>
+                          </Tooltip>
+                        )}
+                        <Tooltip title="Copy API Key">
+                          <div>
+                            <IconButton
+                              type="button"
+                              onClick={() => {
+                                navigator.clipboard.writeText(apiKey);
+                                notification.success({ ...NOTIFICATIONS_CONFIG.success, message: 'Copied to clipboard' });
+                              }}
+                            >
+                              <Copy color="#252f4a" size={18} />
+                            </IconButton>
+                          </div>
+                        </Tooltip>
+                        <Tooltip title="Refresh API Key">
+                          <Popconfirm onConfirm={handleRefreshApiKey} title="This will invalidate current key and issue a new one">
+                            <div>
+                              <IconButton type="button" loading={refreshingApiKey}>
+                                <RefreshCw color="#252f4a" size={18} />
+                              </IconButton>
+                            </div>
+                          </Popconfirm>
+                        </Tooltip>
+                      </Row>
+                    </div>
+                  </Row>
+                  <p>Secret keys grants access to the API. Keep key safe and do not expose it.</p>
+                </Wrapper>
+              ) : (
+                <Wrapper>
+                  <h5 className="m-0">Setup API Key</h5>
+                  <Button type="button" onClick={handleRefreshApiKey} loading={refreshingApiKey} theme="TERTIARY">
+                    Generate API Key
+                  </Button>
+                  <p className="m-0">
+                    Let researchers find articles using the API. The researcher has to pass this key into all API requests as
+                    <code>X-API-KEY={'<API_key>'}</code> header.
+                  </p>
+                </Wrapper>
+              )}
+            </Tabs.TabPane>
           )}
-        </Row>
+        </Tabs>
 
         <div className={s['ds-modal-form__buttons']}>
           <Button loading={loading} theme="CTA">
