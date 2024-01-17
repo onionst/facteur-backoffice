c4057c751bb77c69efac1e55ff2775237149c485
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2024-01-17 11:06:23 +0100

    Feat (Portrait) preview added

--- bases/Image/Image.tsx
+++ bases/Image/Image.tsx
@@ -1,16 +1,45 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { DetailedHTMLProps, ImgHTMLAttributes, useEffect, useState } from 'react';
 import s from './Image.module.scss';
+import { FileType } from '@/components/EE24Search/EE24Search';
+import { FILE_TYPES } from '@/constants/accept';
 
 export type ImageProps = {
   defaultImage?: string;
   alt: string;
+  src: string;
 };
 
 export default function Image(props: DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & ImageProps) {
+  const [defaultPortrait, setDefaultPortrait] = useState<string>('/assets/portraits/image.svg');
   const [src, setSrc] = useState<string>('');
   useEffect(() => {
-    setSrc(props.src || props.defaultImage || '/assets/portraits/image.svg');
+    const url = props.src;
+    let portraitType: FileType = 'NONE';
+    FILE_TYPES.images.forEach(ext => {
+      if (url.includes(ext)) {
+        portraitType = 'IMAGE';
+      }
+    });
+    FILE_TYPES.audio.forEach(ext => {
+      if (url.includes(ext)) {
+        portraitType = 'AUDIO';
+      }
+    });
+    FILE_TYPES.videos.forEach(ext => {
+      if (url.includes(ext)) {
+        portraitType = 'VIDEO';
+      }
+    });
+    // eslint-disable-next-line no-shadow
+    const portrait = {
+      NONE: '',
+      IMAGE: url,
+      AUDIO: '/assets/portraits/audio.svg',
+      VIDEO: '/assets/portraits/video.svg'
+    }[portraitType];
+    setDefaultPortrait(portrait);
+    setSrc(props.src || portrait || props.defaultImage || '/assets/portraits/image.svg');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.src]);
 
@@ -20,7 +49,7 @@ export default function Image(props: DetailedHTMLProps<ImgHTMLAttributes<HTMLIma
       {...props}
       className={`${s['ds-image']} ${props.className || ''}`}
       src={src}
-      onError={() => setSrc(props.defaultImage || '/assets/portraits/image.svg')}
+      onError={() => setSrc(defaultPortrait || props.defaultImage || '/assets/portraits/image.svg')}
     />
   );
 }
