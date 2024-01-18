d8911262e71fc4a909d67e3f7e6eaa52a3a82a80
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2024-01-18 13:02:04 +0100

    Feat (Stats) iframe added

--- pages/app/chatbot.tsx
+++ pages/app/chatbot.tsx
@@ -1,7 +1,6 @@
 import { MessageCircle } from 'react-feather';
 import Frame from '@/components/Frame/Frame';
 import Header from '@/components/Header/Header';
-import Page from '@/components/Page/Page';
 import Wrapper from '@/components/Wrapper/Wrapper';
 import { SETTINGS } from '@/constants/settings';
 
@@ -10,9 +9,7 @@ export default function Chatbot() {
     <>
       <Header icon={<MessageCircle />} title="Chatbot" />
       <Wrapper>
-        <Page>
-          <Frame src={SETTINGS.PUBLIC_CHATBOT_URL} />
-        </Page>
+        <Frame src={SETTINGS.PUBLIC_CHATBOT_URL} />
       </Wrapper>
     </>
   );
