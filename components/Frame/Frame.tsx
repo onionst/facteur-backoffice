57fe182348a185d843ed873bb92b626456567052
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2024-01-18 16:33:17 +0100

    Feat (Iframe) frame loading added

--- components/Frame/Frame.tsx
+++ components/Frame/Frame.tsx
@@ -1,6 +1,32 @@
+import { Skeleton } from 'antd';
+import { useState } from 'react';
 import s from './Frame.module.scss';
 export type FrameProps = { src: string };
 
 export default function Frame(props: FrameProps) {
-  return <iframe src={props.src} className={s['ds-frame']} />;
+  const [loading, setLoading] = useState<boolean>(true);
+  return (
+    <div className={s['ds-frame-container']}>
+      {loading && (
+        <div className={s['ds-frame--loading']}>
+          <div>
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+            <Skeleton active />
+          </div>
+        </div>
+      )}
+      <iframe
+        src={props.src}
+        onLoad={() => {
+          setLoading(false);
+        }}
+        className={s['ds-frame']}
+      />
+    </div>
+  );
 }
