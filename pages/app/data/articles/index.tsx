184acabb420ec673774415657593545c684fa110
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2024-01-18 11:12:01 +0100

    Feat (Articles) table  fixed

--- pages/app/data/articles/index.tsx
+++ pages/app/data/articles/index.tsx
@@ -66,7 +66,7 @@ export default function Articles() {
             columns={[
               'Headline',
               'URL',
-
+              'Type',
               <Sorter
                 key="Sorter"
                 onSort={() => {
