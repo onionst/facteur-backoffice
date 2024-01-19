b7f21c806bdff5cce81a63402a5a041b96fecab2
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2024-01-19 14:00:33 +0100

    Feat (UX) search updated

--- components/Form/ArticleDrafts/EditArticleDraftForm.tsx
+++ components/Form/ArticleDrafts/EditArticleDraftForm.tsx
@@ -21,10 +21,13 @@ import { LanguageISO } from '@/constants/language';
 import { Topic } from '@/constants/topics';
 import { WorldCountriesISO } from '@/constants/worldCountries';
 import { useArticles } from '@/contexts/articles.context';
+import { validateUrl } from '@/utils/validateUrl';
 
 export type ArticleDraftFormProps = {};
 export default function EditArticleDraftForm(props: ArticleDraftFormProps & IArticleDraft) {
-  const { fetchTranslation } = useArticles();
+  const { fetchTranslation, fetchMetadata } = useArticles();
+  const [fetchingUrlMetadata, setFetchingUrlMetadata] = useState<boolean>(false);
+  const [urlFetcheable, setUrlFetcheable] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const { setForm, form } = props;
   const handleUpdate = (v: any, k: string) => setForm((prev: any) => ({ ...prev, [k]: v.target.value }));
@@ -43,6 +46,25 @@ export default function EditArticleDraftForm(props: ArticleDraftFormProps & IArt
       setLoading(false);
     }
   };
+
+  const handleFetchUrlMetadata = async () => {
+    try {
+      setFetchingUrlMetadata(true);
+      const { metadata } = await fetchMetadata(props.type, form.url);
+      const keywords = metadata?.keywords?.length > 0 ? metadata?.keywords?.filter((i: any) => i?.length > 3) : [];
+      setForm((prev: any) => ({
+        ...prev,
+        description: metadata?.summary || '',
+        headlineNative: metadata?.title || prev?.headlineNative,
+        image: metadata?.image || prev?.image,
+        datePublished: dayjs(metadata?.date).isValid() ? dayjs(metadata?.date) : prev?.datePublished,
+        keywords: keywords?.length > 0 ? keywords : prev?.keywords
+      }));
+      setFetchingUrlMetadata(false);
+    } catch (err) {
+      setFetchingUrlMetadata(false);
+    }
+  };
   return (
     <form className={s['ds-article-draft-form']} onSubmit={handleSubmit}>
       <Page>
@@ -54,18 +76,26 @@ export default function EditArticleDraftForm(props: ArticleDraftFormProps & IArt
         <Card>
           <h4>Overview</h4>
           <Divider style={{ margin: '8px 0' }} />
-          <Input
-            label={`URL of the ${props.type === ArticleType.Narrative ? 'report' : 'article'}`}
-            type="url"
-            name="url"
-            minLength={10}
-            value={form.url}
-            onChange={v => handleUpdate(v, 'url')}
-            id="url"
-            pattern="[Hh][Tt][Tt][Pp][Ss]?:\/\/(?:(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,}))(?::\d{2,5})?(?:\/[^\s]*)?"
-            required
-            placeholder="https://example.com/factchecking/article-010101"
-          />
+          <div className="w-full ds-buttons-flex">
+            <Input
+              label={`URL of the ${props.type === ArticleType.Narrative ? 'report' : 'article'}`}
+              type="url"
+              name="url"
+              minLength={10}
+              value={form.url}
+              onChange={v => {
+                handleUpdate(v, 'url');
+                setUrlFetcheable(validateUrl(v.target.value));
+              }}
+              id="url"
+              pattern="[Hh][Tt][Tt][Pp][Ss]?:\/\/(?:(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,}))(?::\d{2,5})?(?:\/[^\s]*)?"
+              required
+              placeholder="https://example.com/factchecking/article-010101"
+            />
+            <Button type="button" onClick={handleFetchUrlMetadata} loading={fetchingUrlMetadata} disabled={!urlFetcheable} theme="TERTIARY">
+              Fetch data
+            </Button>
+          </div>
           <Input
             type="text"
             minLength={10}
--- components/Form/ArticleDrafts/EditDebunkArticleDraftForm.tsx
+++ components/Form/ArticleDrafts/EditDebunkArticleDraftForm.tsx
@@ -25,11 +25,14 @@ import { ReviewRating } from '@/constants/ratings';
 import { Topic } from '@/constants/topics';
 import { WorldCountriesISO } from '@/constants/worldCountries';
 import { useArticles } from '@/contexts/articles.context';
+import { validateUrl } from '@/utils/validateUrl';
 
 export type DebunkArticleDraftFormProps = {};
 export default function EditDebunkArticleDraftForm(props: DebunkArticleDraftFormProps & IArticleDraft) {
   const [loading, setLoading] = useState<boolean>(false);
-  const { fetchTranslation } = useArticles();
+  const { fetchTranslation, fetchMetadata } = useArticles();
+  const [fetchingUrlMetadata, setFetchingUrlMetadata] = useState<boolean>(false);
+  const [urlFetcheable, setUrlFetcheable] = useState<boolean>(false);
   const { setForm, form } = props;
   const handleUpdate = (v: any, k: string) => setForm((prev: any) => ({ ...prev, [k]: v.target.value }));
 
@@ -53,6 +56,25 @@ export default function EditDebunkArticleDraftForm(props: DebunkArticleDraftForm
     }
   };
 
+  const handleFetchUrlMetadata = async () => {
+    try {
+      setFetchingUrlMetadata(true);
+      const { metadata } = await fetchMetadata(props.type, form.url);
+      const keywords = metadata?.keywords?.length > 0 ? metadata?.keywords?.filter((i: any) => i?.length > 3) : [];
+      setForm((prev: any) => ({
+        ...prev,
+        description: metadata?.summary || '',
+        headlineNative: metadata?.title || prev?.headlineNative,
+        image: metadata?.image || prev?.image,
+        datePublished: dayjs(metadata?.date).isValid() ? dayjs(metadata?.date) : prev?.datePublished,
+        keywords: keywords?.length > 0 ? keywords : prev?.keywords
+      }));
+      setFetchingUrlMetadata(false);
+    } catch (err) {
+      setFetchingUrlMetadata(false);
+    }
+  };
+
   return (
     <form className={s['ds-article-draft-form']} onSubmit={handleSubmit}>
       <Page>
@@ -64,18 +86,26 @@ export default function EditDebunkArticleDraftForm(props: DebunkArticleDraftForm
         <Card>
           <h4>Overview</h4>
           <Divider style={{ margin: '8px 0' }} />
-          <Input
-            label={`URL of the ${props.type === ArticleType.Narrative ? 'report' : 'article'}`}
-            type="url"
-            name="url"
-            minLength={10}
-            value={form.url}
-            onChange={v => handleUpdate(v, 'url')}
-            id="url"
-            pattern="[Hh][Tt][Tt][Pp][Ss]?:\/\/(?:(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,}))(?::\d{2,5})?(?:\/[^\s]*)?"
-            required
-            placeholder="https://example.com/factchecking/article-010101"
-          />
+          <div className="w-full ds-buttons-flex">
+            <Input
+              label={`URL of the ${props.type === ArticleType.Narrative ? 'report' : 'article'}`}
+              type="url"
+              name="url"
+              minLength={10}
+              value={form.url}
+              onChange={v => {
+                handleUpdate(v, 'url');
+                setUrlFetcheable(validateUrl(v.target.value));
+              }}
+              id="url"
+              pattern="[Hh][Tt][Tt][Pp][Ss]?:\/\/(?:(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,}))(?::\d{2,5})?(?:\/[^\s]*)?"
+              required
+              placeholder="https://example.com/factchecking/article-010101"
+            />
+            <Button type="button" onClick={handleFetchUrlMetadata} loading={fetchingUrlMetadata} disabled={!urlFetcheable} theme="TERTIARY">
+              Fetch data
+            </Button>
+          </div>
           <Input
             type="text"
             minLength={10}
