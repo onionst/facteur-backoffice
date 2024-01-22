cf8a7577d269d7c41d579f048e72194cc6d433ac
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2024-01-22 19:52:14 +0100

    Fix (EFCSN-33) Filter list updated

--- utils/convertJsonToCsv.ts
+++ utils/convertJsonToCsv.ts
@@ -1,14 +1,14 @@
 export const convertJsonToCsv = (jsonData: any[]) => {
   const csvRows = [];
   const headers = Object.keys(jsonData[0]);
-  csvRows.push(headers.join(','));
+  csvRows.push(headers.join(';'));
 
   for (const row of jsonData) {
     const values = headers.map(header => {
       const escaped = ('' + row[header]).replace(/"/g, '\\"');
       return `"${escaped}"`;
     });
-    csvRows.push(values.join(','));
+    csvRows.push(values.join(';'));
   }
 
   return new Blob([csvRows.join('\n')], { type: 'text/csv' });
