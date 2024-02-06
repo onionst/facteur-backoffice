9aafb381898a99104e4ac56cbd928d716f0c1ca2
Bruno Passarelli <bruno.passarelli@MacBook-Pro-Bruno.local>
2024-02-06 15:26:07 +0100

    Fix (Envs) runtime env safe split added

--- constants/settings.ts
+++ constants/settings.ts
@@ -10,5 +10,5 @@ export const SETTINGS = {
   PUBLIC_CHATBOT_URL: publicRuntimeConfig.PUBLIC_CHATBOT_URL + '/api/chat',
   PUBLIC_STATS_URL: publicRuntimeConfig.PUBLIC_STATS_URL,
   PUBLIC_TRENDS_URL: publicRuntimeConfig.PUBLIC_TRENDS_URL,
-  PUBLIC_SECTIONS: publicRuntimeConfig.PUBLIC_SECTIONS.split(',').map((section: string) => section.trim().toLowerCase())
+  PUBLIC_SECTIONS: (publicRuntimeConfig.PUBLIC_SECTIONS || '').split(',').map((section: string) => section.trim().toLowerCase())
 };
