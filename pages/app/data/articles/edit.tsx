c799b2b76c4a304ad2aa1d8bdda4ddb01fd047a2
Pablo Alvarez <pablo.alvarez@newtral.es>
2024-03-15 11:41:58 +0100

    fix(): an error notification appears when a superadmin edit an article and afterfetch all articles

--- pages/app/data/articles/edit.tsx
+++ pages/app/data/articles/edit.tsx
@@ -13,6 +13,7 @@ import Header from '@/components/Header/Header';
 import Stepper from '@/components/Stepper/Stepper';
 import Wrapper from '@/components/Wrapper/Wrapper';
 import { MIN_LENGTH_KEYWORDS } from '@/constants/accept';
+import { ROLES } from '@/constants/roles.constants';
 import { useArticles } from '@/contexts/articles.context';
 import { useAuth } from '@/contexts/auth.context';
 import { useHistory } from '@/contexts/history.context';
@@ -178,9 +179,13 @@ export default function Edit() {
 
       await updateArticle(Object.fromEntries(Object.entries(payload).filter(v => v[1] != null)));
 
-      fetchArticles({
-        publisher: session.organization?.domain
-      });
+      if ([ROLES.ADMIN, ROLES.FACT_CHECKER].includes(session?.role)) {
+        fetchArticles({
+          publisher: session.organization?.domain
+        });
+      } else {
+        fetchArticles();
+      }
       if (router?.query?.f === 'search') {
         router.push('/app/ee24/search');
       } else {
