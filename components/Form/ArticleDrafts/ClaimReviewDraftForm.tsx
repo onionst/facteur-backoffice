d5bfd87232c87b3088b44438e31362d13bcd3a00
César Landesa Gómez <cesarlan@glue.digital>
2025-02-20 15:06:07 +0100

    Added missing fields to edit and new forms and fixed bug with translation

--- components/Form/ArticleDrafts/ClaimReviewDraftForm.tsx
+++ components/Form/ArticleDrafts/ClaimReviewDraftForm.tsx
@@ -10,7 +10,9 @@ import Tagger from '@/bases/Tagger/Tagger';
 import { TextArea } from '@/bases/Textarea';
 import Card from '@/components/Card/Card';
 import { AiVerificationType } from '@/constants/aiVerification';
+import { ClaimantInfluence, ClaimantType } from '@/constants/climant';
 import { DistortionType } from '@/constants/distortionType';
+import { HarmEscalation } from '@/constants/harmEscalation';
 import { MediaFormat, Platform } from '@/constants/media';
 import { ReviewRating } from '@/constants/ratings';
 
@@ -99,8 +101,44 @@ export default function ClaimReviewDraftForm(props: {
           }
         ]}
         onChange={v => props.handleUpdate({ ...claimReview, multiclaim: v === 'true' })}
-        required
       />
+      <Row align="SPACE">
+        <Select
+          disabled={preview}
+          label="Harm"
+          defaultValue={claimReview?.harm ? 'true' : claimReview?.harm === false ? 'false' : ''}
+          options={[
+            { label: 'Select harm', value: '' },
+            {
+              label: 'Yes',
+              value: 'true'
+            },
+            {
+              label: 'No',
+              value: 'false'
+            }
+          ]}
+          onChange={v => props.handleUpdate({ ...claimReview, harm: v === 'true' ? true : v === 'false' ? false : undefined })}
+        />
+        <Select
+          label="Harm escalation"
+          defaultValue={claimReview?.harmEscalation}
+          options={[
+            { label: 'Select the harm escalation', value: '' },
+            ...Object.entries(HarmEscalation).map(([, value]) => ({
+              label: value.split('_').join(' '),
+              value: value.split('_').join(' ')
+            }))
+          ]}
+          onChange={v =>
+            props.handleUpdate({
+              ...claimReview,
+              harmEscalation: v
+            })
+          }
+          disabled={preview}
+        />
+      </Row>
       <Tagger
         value={claimReview?.distortionType}
         options={[
@@ -263,6 +301,171 @@ export default function ClaimReviewDraftForm(props: {
                   }
                   disabled={preview}
                 />
+                <Row align="SPACE">
+                  <Input
+                    label="Views"
+                    type="number"
+                    key={`${props.formUrl}_appearance_${appearanceIndex}_views`}
+                    value={appearance?.views}
+                    onChange={v =>
+                      props.handleUpdate({
+                        ...claimReview,
+                        appearances: claimReview.appearances.map((_appearance: any, _appearanceIndex: any) => {
+                          if (_appearanceIndex === appearanceIndex) {
+                            return {
+                              ..._appearance,
+                              views: v.target.value
+                            };
+                          }
+                          return _appearance;
+                        })
+                      })
+                    }
+                    disabled={preview}
+                  />
+                  <Input
+                    label="Likes"
+                    type="number"
+                    key={`${props.formUrl}_appearance_${appearanceIndex}_likes`}
+                    value={appearance?.likes}
+                    onChange={v =>
+                      props.handleUpdate({
+                        ...claimReview,
+                        appearances: claimReview.appearances.map((_appearance: any, _appearanceIndex: any) => {
+                          if (_appearanceIndex === appearanceIndex) {
+                            return {
+                              ..._appearance,
+                              likes: v.target.value
+                            };
+                          }
+                          return _appearance;
+                        })
+                      })
+                    }
+                    disabled={preview}
+                  />
+                  <Input
+                    label="Comments"
+                    type="number"
+                    key={`${props.formUrl}_appearance_${appearanceIndex}_comments`}
+                    value={appearance?.comments}
+                    onChange={v =>
+                      props.handleUpdate({
+                        ...claimReview,
+                        appearances: claimReview.appearances.map((_appearance: any, _appearanceIndex: any) => {
+                          if (_appearanceIndex === appearanceIndex) {
+                            return {
+                              ..._appearance,
+                              comments: v.target.value
+                            };
+                          }
+                          return _appearance;
+                        })
+                      })
+                    }
+                    disabled={preview}
+                  />
+                  <Input
+                    label="Shares"
+                    type="number"
+                    key={`${props.formUrl}_appearance_${appearanceIndex}_shares`}
+                    value={appearance?.shares}
+                    onChange={v =>
+                      props.handleUpdate({
+                        ...claimReview,
+                        appearances: claimReview.appearances.map((_appearance: any, _appearanceIndex: any) => {
+                          if (_appearanceIndex === appearanceIndex) {
+                            return {
+                              ..._appearance,
+                              shares: v.target.value
+                            };
+                          }
+                          return _appearance;
+                        })
+                      })
+                    }
+                    disabled={preview}
+                  />
+                </Row>
+                <Row align="SPACE">
+                  <Input
+                    label="Claimant"
+                    type="text"
+                    key={`${props.formUrl}_appearance_${appearanceIndex}_claimant`}
+                    value={appearance?.claimant}
+                    onChange={v =>
+                      props.handleUpdate({
+                        ...claimReview,
+                        appearances: claimReview.appearances.map((_appearance: any, _appearanceIndex: any) => {
+                          if (_appearanceIndex === appearanceIndex) {
+                            return {
+                              ..._appearance,
+                              claimant: v.target.value
+                            };
+                          }
+                          return _appearance;
+                        })
+                      })
+                    }
+                    disabled={preview}
+                  />
+                </Row>
+                <Row align="SPACE">
+                  <Select
+                    label="Claimant type"
+                    defaultValue={appearance?.claimantType}
+                    key={`${props.formUrl}_appearance_${appearanceIndex}_claimantType`}
+                    options={[
+                      { label: 'Select the claimant type', value: '' },
+                      ...Object.entries(ClaimantType).map(([key, value]) => ({
+                        label: key.split('_').join(' '),
+                        value: value.split('_').join(' ')
+                      }))
+                    ]}
+                    onChange={v =>
+                      props.handleUpdate({
+                        ...claimReview,
+                        appearances: claimReview.appearances.map((_appearance: any, _appearanceIndex: any) => {
+                          if (_appearanceIndex === appearanceIndex) {
+                            return {
+                              ..._appearance,
+                              claimantType: v
+                            };
+                          }
+                          return _appearance;
+                        })
+                      })
+                    }
+                    disabled={preview}
+                  />
+                  <Select
+                    label="Claimant influence"
+                    defaultValue={appearance?.claimantInfluence}
+                    key={`${props.formUrl}_appearance_${appearanceIndex}_claimantInfluence`}
+                    options={[
+                      { label: 'Select the claimant influence', value: '' },
+                      ...Object.entries(ClaimantInfluence).map(([key, value]) => ({
+                        label: key.split('_').join(' '),
+                        value: value.split('_').join(' ')
+                      }))
+                    ]}
+                    onChange={v =>
+                      props.handleUpdate({
+                        ...claimReview,
+                        appearances: claimReview.appearances.map((_appearance: any, _appearanceIndex: any) => {
+                          if (_appearanceIndex === appearanceIndex) {
+                            return {
+                              ..._appearance,
+                              claimantInfluence: v
+                            };
+                          }
+                          return _appearance;
+                        })
+                      })
+                    }
+                    disabled={preview}
+                  />
+                </Row>
                 {!preview && appearanceIndex !== 0 && (
                   <Row align="RIGHT">
                     <span
