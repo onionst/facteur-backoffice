5121b8325a44923ba2e726825b15e5309cbdb70a
Pablo Toca Berdejo <pablotocaberdejo@MacBook-Pro.local>
2025-03-17 21:25:51 +0100

    fix(CHEC-59): fix if

--- contexts/auth.context.tsx
+++ contexts/auth.context.tsx
@@ -336,7 +336,7 @@ export const AuthProvider = (props: AuthProviderProps) => {
           message: 'Error',
           description: message ?? 'Invalid google access token'
         });
-      } else if (err.message === "User has no permissons" || err.message === "User does not exists") {
+      } else if (err.message === "User has no permissons" || err.message === "User does not exist") {
         notification.error({
           ...NOTIFICATIONS_CONFIG.error,
           message: 'Error',
